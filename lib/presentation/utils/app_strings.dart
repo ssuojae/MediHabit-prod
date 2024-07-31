dfca5738b4ac9b7743e3506f6c99b16ec9d09f2c
suojae3 <suojae3@gmail.com>
2024-07-31 17:50:37 +0900

    :sparkles: app string 추가

---
+++ lib/presentation/utils/app_strings.dart
@@ -0,0 +1,9 @@
+final class AppStrings {
+  
+  // loginview.dart
+  static const headerText = '나의 복용 습관을 지켜주는';
+  static const titleText = '메디헤빗';
+  static const kakaoLogin = '카카오톡으로 로그인';
+  static const appleLogin = 'Apple로 로그인';
+  static const googleLogin = 'Google로 로그인';
+}
