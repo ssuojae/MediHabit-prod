221fee1bf7f9331fdf7d1ba1e2b325458d62b50d
suojae3 <suojae3@gmail.com>
2024-07-31 17:49:42 +0900

    :sparkles: feat: app image 추가

---
+++ lib/presentation/utils/app_images.dart
@@ -0,0 +1,7 @@
+final class AppImages {
+  
+  // loginview.dart
+  static const kakaoIcon = 'images/kakao_icon.png';
+  static const appleIcon = 'images/apple_icon.png';
+  static const googleIcon = 'images/google_icon.png';
+}
