4b9c85989de99517a56c33fa76dc7cb4c9c6b150
suojae3 <suojae3@gmail.com>
2024-07-31 17:49:59 +0900

    :sparkles: feat: app size 추가

---
+++ lib/presentation/utils/app_sizes.dart
@@ -0,0 +1,15 @@
+final class AppSizes {
+  
+  // button.dart
+  static const double loginButtonVerticalInset = 12.0;
+  static const double loginButtonHorizontalInset = 24.0;
+
+  // loginview.dart
+  static const double headerFontSize = 24.0;
+  static const double titleFontSize = 36.0;
+  static const double topSectionHeight = 300.0;
+  static const double bottomSectionHeight = 200.0;
+  static const double spacerHeight = 50.0;
+  static const double paddingBottom = 50.0;
+
+}
