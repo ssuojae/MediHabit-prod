156e2c4c4148aa946a94e5bc4f29247e640be538
suojae3 <suojae3@gmail.com>
2024-07-31 17:51:38 +0900

    :sparkles: feat: login view 추가

---
+++ lib/presentation/login_scene/view/loginview.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:medihabit/presentation/login_scene/viewmodel/login_viewmodel.dart';
+import 'package:medihabit/presentation/utils/ui_utils.dart';
+
+
+final class LoginView extends StatelessWidget {
+  final LoginViewModel loginViewModel;
+
+  const LoginView({super.key, required this.loginViewModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: AppSizes.topSectionHeight,
+          child: _TopSection(),
+        ),
+        SizedBox(
+          height: AppSizes.bottomSectionHeight,
+          child: _BottomSection(loginViewModel: loginViewModel),
+        ),
+      ],
+    );
+  }
+}
+
+final class _TopSection extends StatelessWidget {
+  static const _headerText = Text(
+    AppStrings.headerText,
+    textAlign: TextAlign.center,
+    style: TextStyle(
+      fontSize: AppSizes.headerFontSize,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+
+  static const _titleText = Text(
+    AppStrings.titleText,
+    textAlign: TextAlign.center,
+    style: TextStyle(
+      fontSize: AppSizes.titleFontSize,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _headerText,
+          SizedBox(height: AppSizes.spacerHeight),
+          _titleText,
+        ],
+      ),
+    );
+  }
+}
+
+final class _BottomSection extends StatelessWidget {
+  final LoginViewModel loginViewModel;
+
+  const _BottomSection({required this.loginViewModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.only(bottom: AppSizes.paddingBottom),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AppButtons.loginButton(
+            onPressed: loginViewModel.authenticateWithGoogle,
+            backgroundColor: Colors.blue,
+            textColor: Colors.white,
+            text: AppStrings.googleLogin,
+            icon: const ImageIcon(
+              AssetImage(AppImages.googleIcon),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
