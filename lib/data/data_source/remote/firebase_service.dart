00ddd4beeeff92fcc6ac95ac204e1b9ad9d97c44
suojae3 <suojae3@gmail.com>
2024-08-02 17:21:32 +0900

    :sparkles: feat: _saveUserToFirestore 메서드 추가

--- lib/data/data_source/remote/firebase_service.dart
+++ lib/data/data_source/remote/firebase_service.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 final class FirebaseService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<UserCredential> signInWithGoogle() async {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
@@ -10,6 +13,20 @@ final class FirebaseService {
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+    UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+
+    await _saveUserToFirestore(userCredential.user);
+
+    return userCredential;
+  }
+
+  Future<void> _saveUserToFirestore(User? user) async {
+    if (user == null) return;
+
+    final userRef = _firestore.collection('users').doc(user.uid);
+    await userRef.set({
+      'name': user.displayName,
+      'email': user.email,
+    }, SetOptions(merge: true));
   }
 }
